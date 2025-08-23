ccc114998d8679b788029bd01656a22d18b41088
Alson Dabo <alsondab9@gmail.com>
2025-08-23 00:59:29 +0000

    first commit

--- .history/app/page_20250823005632.tsx
+++ .history/app/page_20250823005632.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <Button>Button</Button>
       </main>
+
     </div>
   );
 }
